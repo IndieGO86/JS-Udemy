0e994a9f39927b35d9ea3ecb4d33b416c8c028a1
IndieGO <example@gmail.com>
2022-11-09 22:05:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,141 @@
+"use strict";
+
+// const venicleBodyWidth = 5000;
+// const venicleBodyLength = 4400;
+
+// console.log(" widht box: " + venicleBodyWidth + ", large: " + venicleBodyLength);
+
+// ['jsuybd', 'sdssdasd', 'sdsdsda'].map(a =>)
+
+
+
+// snake_case
+// UPPER_SNAKE_CASE
+
+// const userName = 'John';
+// let userNumber = 25;
+
+// userNumber = 24;
+
+
+// let number = 4;
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const person = "MAx";
+
+// const bool = true;
+
+// console.log(something);
+
+////////////////////////////////OBject///////////////////////
+// let und;
+
+// console.log(und);
+
+// const obj = {
+//     name: "Max",
+//     age: 25,
+//     isMarried: false
+// };
+// // console.log(obj.age);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', '5', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[2]);
+
+////////////////////////////////OBject & Massiv///////////////////////
+
+// const arr = [1, 3, 5];
+
+// arr[10] = "232113";
+
+
+// const arrObj = {
+//     a: 1,
+//     b: 3,
+//     c: 5,
+//     abc: {
+//         name: "alex",
+//         age: 24,
+//     }
+// };
+
+// const d = "d";
+
+// // arrObj.d = "1231";
+// arrObj[d] = "1231";
+
+
+// console.log(arrObj);
+// console.log(arr);
+ 
+// const storeName = 'SnowInn';
+// const storeDescription = {
+//     budget: 1000,
+//     employees: ['max', 'anna', 'tom'],
+//     products: {
+//         ski: 5000,
+//         bord: 4000
+//     },
+//     open: true
+// };   
+
+// console.log(storeDescription);
+
+////////////////////////////////comand///////////////////////
+
+// alert("hello");
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('How are you old?', '');
+// console.log(answer+5);
+
+// const answer = [];
+
+// // answer[0] = prompt('whats you first name?', '');
+// // answer[1] = prompt('whats you last name?', '');
+// // answer[2] = prompt('How old are you?', '');
+
+// console.log(typeof(answer));
+
+
+////////////////////////////////интреполяция///////////////////////
+
+// const category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Hi, ${user}`);
+
+////////////////////////////////операторы///////////////////////
+
+// console.log(4 + '4');
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2 + 2 * 2 === '6');
+
+// const isChecked = true,
+//       isClose = false;
+
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+////////////////////////////////операторы///////////////////////
