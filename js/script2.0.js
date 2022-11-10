3908bdf2972d3f658491ba2dd57a57319cfac90a
IndieGO <example@gmail.com>
2022-11-10 17:58:05 +0300

    первое задание в новый файл

--- js/script2.0.js
+++ js/script2.0.js
@@ -1,3 +1,22 @@
-const numberOfFilms = +prompt('How films you wath?');
-console.log(numberOfFilms);
+const numberOfFilms = +prompt('How films you wath?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из последних просмотрелнных фильмов?'),
+      b = prompt('На сколько оцените его'),
+      c = prompt('Один из последних просмотрелнных фильмов?'),
+      d = prompt('На сколько оцените его');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
